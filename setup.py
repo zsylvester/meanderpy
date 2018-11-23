0cad81d3aea5b9fce2e499103f3b2388cbeb32a3
Zoltan Sylvester <zoltan.sylvester@gmail.com>
2018-11-23 15:58:08 -0600

    minor edit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="meanderpy",
-    version="0.0.1",
+    version="1.0.0",
     author="Zoltan Sylvester",
     author_email="zoltan.sylvester@beg.utexas.edu",
     description="A simple model of meander migration",
